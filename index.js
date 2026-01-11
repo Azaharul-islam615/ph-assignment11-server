d1a98d42ebcb9c2afd7eeb3d85d7a94de26e9301
Azaharul-islam615 <mdazaharul725@gmail.com>
2026-01-12 02:47:39 +0600

    update data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const stripe = require('stripe')(process.env.STRIPE_SCRETE);
 
 const app = express()
 const port = process.env.PORT || 3000
